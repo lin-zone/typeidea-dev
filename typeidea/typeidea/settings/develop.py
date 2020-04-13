831d6260a087d7963b40fe83852572df4c2034fd
lin-zone <z_one10@163.com>
2020-04-14 03:14:42 +0800

    初始化提交

---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,23 @@
+from .base import *     # NOQA
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.11/topics/i18n/
+
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
+
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
